54b5e7acad83c199a105b5297995a63ca8320ef5
umuntu02 <irumvadevlin98@gmail.com>
2024-11-19 04:27:24 +0100

    test with pbw2 layouts

--- app/Views/layouts/base.php
+++ app/Views/layouts/base.php
@@ -1,33 +1,35 @@
-<?php require __DIR__ . '/../_types.php'?>
+<?php require __DIR__ . '/../_types.php' ?>
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?="{$pageTitle}"?> | Boutique en Ligne</title>
-        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= "{$pageTitle}" ?> | Boutique en Ligne</title>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
 
-        <link rel="stylesheet" href="/public/assets/css/style.css">
-    </head>
+    <link rel="stylesheet" href="/public/assets/css/style.css">
+</head>
 
-    <body class="dark">
-    <?php $view->renderSection('nav');?>
+<body>
+    <div class="big-wrapper .light">
+        <?php $view->renderSection('nav'); ?>
 
 
 
-    <div class="bottom-area">
-          <div class="container">
-            <button class="toggle-btn">
-              <i class="far fa-moon"></i>
-              <i class="far fa-sun"></i>
-            </button>
-          </div>
+        <div class="bottom-area">
+            <div class="container">
+                <button class="toggle-btn">
+                    <i class="far fa-moon"></i>
+                    <i class="far fa-sun"></i>
+                </button>
+            </div>
         </div>
 
 
         <script src="/public/assets/js/script.js"></script>
-    </body>
+    </div>
+</body>
 
 </html>
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,11 +1,12 @@
-// Script for navigation bar
 // Select The Elements
 var toggle_btn;
-
+var big_wrapper;
+var hamburger_menu;
 
 function declare() {
   toggle_btn = document.querySelector(".toggle-btn");
-
+  big_wrapper = document.querySelector(".big-wrapper");
+  hamburger_menu = document.querySelector(".hamburger-menu");
 }
 
 const main = document.querySelector("main");
@@ -32,6 +33,7 @@ function toggleAnimation() {
 
   clone.addEventListener("animationend", () => {
     document.body.classList.remove("stop-scrolling");
+    big_wrapper.remove();
     clone.classList.remove("copy");
     // Reset Variables
     declare();
@@ -41,6 +43,9 @@ function toggleAnimation() {
 
 function events() {
   toggle_btn.addEventListener("click", toggleAnimation);
+  hamburger_menu.addEventListener("click", () => {
+    big_wrapper.classList.toggle("active");
+  });
 }
 
 events();
