e818c22472e913920debb97bd92ce078a0926a7b
umuntu02 <irumvadevlin98@gmail.com>
2024-11-19 15:42:12 +0100

    hero first design and favicon add

--- app/Views/layouts/base.php
+++ app/Views/layouts/base.php
@@ -7,6 +7,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= "{$pageTitle}" ?> | Boutique en Ligne</title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/public/assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/public/assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/public/assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/public/assets/favicon/site.webmanifest">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
 
     <link rel="stylesheet" href="/public/assets/css/style.css">
