2a51ea830e9baad234ca9a4530944e95eb037f7d
umuntu02 <irumvadevlin98@gmail.com>
2024-11-19 05:16:02 +0100

    nav bar changed for a better fonction of

--- app/Views/layouts/nav.php
+++ app/Views/layouts/nav.php
@@ -2,28 +2,28 @@
 
 include __DIR__ . '/../_types.php';
 
-function activeClass(string $value)
+function activeClass(string $value, string $currentView)
 {
-    global $viewName;
-    return $viewName == $value ? 'active' : '';
+    return $currentView == $value ? 'active' : '';
 }
 
-?>
-<?php $view->extend('layouts/base');?>
-<?php $view->section('nav');?>
-        <section id="header">
-                    <a href="#"><img src="/public/assets/img/logo.png" class="logo" alt="" ></a>
-                    <div>
-                        <ul id="navbar">
-                            <li><a class="<?= activeClass('home');?>" href="/home">Home</a></li>
-                            <li><a class="<?= activeClass('shop');?>" href="/shop">Shop</a></li>
-                            <li><a class="<?= activeClass('blog');?>" href="/blog">Blog</a></li>
-                            <li><a class="<?= activeClass('about');?>" href="/about">About</a></li>
-                            <li><a class="<?= activeClass('contact');?>" href="/contact">Contact</a></li>
-                            <li><a class="<?= activeClass('cart');?>" href="/cart"><i class="far fa-shopping-bag"></i></a></li>
-                        </ul>
-                    </div>
-                </section>
-        <?php $view->renderSection('body');?>
-<?php $view->endSection();?>
 
+?>
+<?php $view->extend('layouts/base'); ?>
+<?php $view->section('nav'); ?>
+<section id="header">
+    <a href="#"><img src="/public/assets/img/logo.png" class="logo" alt=""></a>
+    <div>
+        <ul id="navbar">
+            <li><a class="<?= activeClass('home', $viewName); ?>" href="/home">Home</a></li>
+            <li><a class="<?= activeClass('shop', $viewName); ?>" href="/shop">Shop</a></li>
+            <li><a class="<?= activeClass('blog', $viewName); ?>" href="/blog">Blog</a></li>
+            <li><a class="<?= activeClass('about', $viewName); ?>" href="/about">About</a></li>
+            <li><a class="<?= activeClass('contact', $viewName); ?>" href="/contact">Contact</a></li>
+            <li><a class="<?= activeClass('cart', $viewName); ?>" href="/cart"><i class="far fa-shopping-bag"></i></a></li>
+        </ul>
+
+    </div>
+</section>
+<?php $view->renderSection('body'); ?>
+<?php $view->endSection(); ?>
