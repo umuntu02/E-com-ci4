0df1f5e428ae152a56238daf01638146036aba6b
umuntu02 <irumvadevlin98@gmail.com>
2024-11-19 15:43:06 +0100

    hero first design and favicon add

--- app/Views/home.php
+++ app/Views/home.php
@@ -3,4 +3,12 @@
 <?php $view->extend('layouts/nav');?>
 
 <?php $view->section('body');?>
+    <section id="hero">
+        <h4>Trade-in-offer</h4>
+        <h2>Super value deals</h2>
+        <h1>On all products</h1>
+        <p>Save more with coupons & up to 70% off !</p>
+        <button>Shop Now</button>
+
+    </section>
 <?php $view->endSection();?>
